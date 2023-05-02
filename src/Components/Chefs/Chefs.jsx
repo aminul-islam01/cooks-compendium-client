3a3cfc63d067fb0243cb379a35497297602a40ce
aminul-islam01 <abiddut953@gmail.com>
2023-05-02 11:22:09 +0600

    add chef section

--- src/Components/Chefs/Chefs.jsx
+++ src/Components/Chefs/Chefs.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
+// import { Card, Col, Row } from 'react-bootstrap';
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
 
-const Chefs = ({chef}) => {
+const Chefs = ({ chef }) => {
+    const { name, picture } = chef;
     console.log(chef)
     return (
-        <div>
-            
-        </div>
+        <Col>
+            <Card>
+                <Card.Img variant="top" src={picture}/>
+                <Card.Body>
+                    <Card.Title>{name}</Card.Title>
+                    <Card.Text>
+                        This is a longer card with supporting text below as a natural
+                        lead-in to additional content. This content is a little bit
+                        longer.
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+        </Col>
     );
 };
 
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Banner from '../../Components/Banner/Banner';
 import { useLoaderData } from 'react-router-dom';
 import Chefs from '../../Components/Chefs/Chefs';
+import { Row } from 'react-bootstrap';
 
 const Home = () => {
     const chefs = useLoaderData()
@@ -9,12 +10,14 @@ const Home = () => {
     return (
         <div>
             <Banner></Banner>
-            {
-                chefs.map(chef => <Chefs
-                    key={chef.id}
-                    chef={chef}>
-                </Chefs>)
-            }
+            <Row xs={1} md={3} className="g-4" className='mt-5'>
+                {
+                    chefs.map(chef => <Chefs
+                        key={chef.id}
+                        chef={chef}>
+                    </Chefs>)
+                }
+            </Row>
         </div>
     );
 };
